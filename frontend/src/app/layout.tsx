25b9e6bc751baabb3e58725812d5c5fb7f880c98
gri-sha <183693051+gri-sha@users.noreply.github.com>
2025-03-20 10:17:55 +0100

    clean up

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import "./globals.css";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -25,8 +25,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
-            <Toaster />
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+                <main>{children}</main>
+                <Toaster />
+            </body>
         </html>
     );
 }
